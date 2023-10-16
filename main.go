15af9c8c249005ee0c3c0f10783574a84a037fbd
BrianMwas <brnmwas@gmail.com>
2023-10-16 09:08:54 +0300

    Initialize mongo db

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	MONGO_URI = "mongodb://%s:%s@mongo:%v/golangmongo?authSource=admin"
+)
+
 func main() {
+	conn := fmt.Sprintf(MONGO_URI, "root", "rootpassword", 8081)
+
+	// Access the client
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(conn))
+	// Capture the errors
+	if err != nil {
+		log.Fatalf("Could not connect to MongoDB: %v", err)
+	}
 }
